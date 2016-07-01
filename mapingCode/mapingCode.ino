85d5b2a2b2acf90edf48f2f5ff1bf46fcf64e39c
Otakar Andrysek <Otakar@MacBook-Pro-2.local>
2016-07-01 12:57:20 -0700

    Switch / case for menu

--- mapingCode/mapingCode.ino
+++ mapingCode/mapingCode.ino
@@ -12,11 +12,46 @@ void setup()
 void loop()
 {
   int cursorPos = analogRead(0);
+  int selection = 0;
   cursorPos = map(cursorPos, 10, 1010, 1,6);
-  lcd.print("Braeden is cool!");
   lcd.setCursor(3,1);
   lcd.print("Value=");
   lcd.print(cursorPos);
   delay(100);
   lcd.clear();
-	}
+  menu(cursorPos);
+  }
+
+ void menu(int cursorPos)
+ {
+  switch (cursorPos) {
+    case 1:
+      lcd.setCursor(0,0);
+      lcd.print("Set Time");
+      break;
+    case 2:
+      lcd.setCursor(0,0);
+      lcd.print("Set close time");
+      break;
+    case 3:
+      lcd.setCursor(0,0);
+      lcd.print("Close on Rain");
+      break;
+    case 4:
+      lcd.setCursor(0,0);
+      lcd.print("Close on ____");
+      break;
+    case 5:
+      lcd.setCursor(0,0);
+      lcd.print("Code on ______");
+      break;
+    case 6:
+      lcd.setCursor(0,0);
+      lcd.print("Shutdown");
+      break;
+    default: 
+      lcd.setCursor(0,0);
+      lcd.print("ERROR");
+    break;
+  }
+ }
